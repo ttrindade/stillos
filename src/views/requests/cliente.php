4d6c89de03bf9efc62e857c12661a32021cd77b4
Thauan <thauantrindade@outlook.com>
2018-09-26 00:46:31 -0300

    cpf mask

--- src/views/requests/cliente.php
+++ src/views/requests/cliente.php
@@ -9,6 +9,58 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+        <script type="text/javascript">
+            function fMasc(objeto, mascara) {
+                obj = objeto
+                masc = mascara
+                setTimeout("fMascEx()", 1)
+            }
+            function fMascEx() {
+                obj.value = masc(obj.value)
+            }
+            function mTel(tel) {
+                tel = tel.replace(/\D/g, "")
+                tel = tel.replace(/^(\d)/, "($1")
+                tel = tel.replace(/(.{3})(\d)/, "$1)$2")
+                if (tel.length == 9) {
+                    tel = tel.replace(/(.{1})$/, "-$1")
+                } else if (tel.length == 10) {
+                    tel = tel.replace(/(.{2})$/, "-$1")
+                } else if (tel.length == 11) {
+                    tel = tel.replace(/(.{3})$/, "-$1")
+                } else if (tel.length == 12) {
+                    tel = tel.replace(/(.{4})$/, "-$1")
+                } else if (tel.length > 12) {
+                    tel = tel.replace(/(.{4})$/, "-$1")
+                }
+                return tel;
+            }
+            function mCNPJ(cnpj) {
+                cnpj = cnpj.replace(/\D/g, "")
+                cnpj = cnpj.replace(/^(\d{2})(\d)/, "$1.$2")
+                cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+                cnpj = cnpj.replace(/\.(\d{3})(\d)/, ".$1/$2")
+                cnpj = cnpj.replace(/(\d{4})(\d)/, "$1-$2")
+                return cnpj
+            }
+            function mCPF(cpf) {
+                cpf = cpf.replace(/\D/g, "")
+                cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+                cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+                cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+                return cpf
+            }
+            function mCEP(cep) {
+                cep = cep.replace(/\D/g, "")
+                cep = cep.replace(/^(\d{2})(\d)/, "$1.$2")
+                cep = cep.replace(/\.(\d{3})(\d)/, ".$1-$2")
+                return cep
+            }
+            function mNum(num) {
+                num = num.replace(/\D/g, "")
+                return num
+            }
+        </script>
     </head>
     <body>
 
@@ -58,7 +110,7 @@
                 </div>
                 <div>
                     <label for="cpf">CPF</label>
-                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
+                    <input type="text" class="form-control" id="cpf" name="cpf" onkeydown="javascript: fMasc(this, mCPF);">
                 </div>
                 <div>
                     <label for="cep">CEP</label>
